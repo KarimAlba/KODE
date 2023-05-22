787a2c13d3d5bafcf89fe0a79f1c9a095bef17e5
Karim <karim_skiy@mail.ru>
2023-05-22 14:59:11 +0300

    mainpage logical

--- src/root/App.tsx
+++ src/root/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.scss';
 import axios from 'axios';
 import TopAppBar from '../components/topAppBar/TopAppBarComponent';
@@ -7,7 +7,6 @@ import MainPage from '../components/mainPage/MainPageComponent';
 import NotFoundPage from '../components/notFoundPage/NotFoundPage';
 import Preloader from '../components/preloader/PreloaderComponent';
 import { Routes, Route, NavLink } from 'react-router-dom';
-import { send } from 'process';
 import Modal from '../components/modalWindow/ModalWindowComponent';
 
 const App = () =>{
@@ -16,15 +15,17 @@ const App = () =>{
   useEffect( () => {
     setTimeout( ()=> {
       setStateOfPreloader(!stateOfPreloader);
-    }, 6000)
+    }, 2000)
   }, []);
 
   const [workers, setWorkers] = useState([]);
   
-  const sendReq = async () => {
-    const responce = await axios.get('https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users?__example=all');
-    const employee = await responce.data.items;
-    return employee;
+  const sendReq = () => {
+    let apiUrl = 'https://stoplight.io/mocks/kode-frontend-team/koder-stoplight/86566464/users?__example=all';
+    axios.get(apiUrl).then((resp) => {
+        let employee = resp.data.items;
+        setWorkers(workers);
+    });
   }
 
   useEffect(() => {
@@ -37,7 +38,7 @@ const App = () =>{
 
   const routes =
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage workers={workers}/>} />
       <Route path='*' element={<NotFoundPage/>} />
     </Routes>
 
@@ -46,7 +47,7 @@ const App = () =>{
       <TopAppBar getStateOfModal={getStateOfModal}/>
       <NavBar/>
       {stateOfModal? <Modal getStateOfModal={getStateOfModal} />: null}
-      {/* {stateOfPreloader? <Preloader /> : routes} */}
+      {stateOfPreloader? <Preloader /> : routes}
     </div>
   );
 }
